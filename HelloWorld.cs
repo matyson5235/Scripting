6fb933003b1f2d835ec612b5f37bfb20838dab07
matyson5235 <matyson5235@gmail.com>
2026-01-05 19:07:19 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hello World");
+    }
+    void Update()
+    {
+        
+    }
+}
